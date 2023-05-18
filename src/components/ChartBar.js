06501ed11e2f9a488349a4375b427d258e694406
Marumo-ProG <Lennythobejane@gmail.com>
2023-05-18 09:10:07 +0200

    started making the month bar-chart animation chart, styles given

---
+++ src/components/ChartBar.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import "../ChartBar.css";
+
+const ChartBar = (props) => {
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="char-bar__fill" ></div>
+            </div>
+            <div className="chart-bar__label">{props.label}</div>
+        </div>
+    )
+}
+
+export default ChartBar;
